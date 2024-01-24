d21a2b44ec0380556b6158ab6bdc1c00b6bf6c66
Gabriel <souzzagabe@hotmail.com>
2024-01-24 16:36:15 -0300

    finish input google

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import Input from "./components/Input"
+import Input from "./components/Input";
 
 function App() {
-
   return (
-
-      <div className="bg-blue-400" >
-          <h1>Clones input</h1>
-          <div>
-            <Input />
-            <Input />
-          </div>
+    <div className="flex flex-col justify-center items-center mt-12 p-3">
+      <h1 className="text-gray-700 font-bold text-2xl mb-6">Clones input</h1>
+      <div className="flex flex-col gap-3 mt-2 w-96">
+        <Input id="name" type="text" placeholder="Nome:" required={true}/>
+        <Input id="email" type="email" placeholder="E-mail:" required={true} />
       </div>
-
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
